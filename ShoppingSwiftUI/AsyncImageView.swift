ff7fa54200163a352c5c47dd13ae41f5d78adc38
Preet Pambhar <preetpambhar73530@gmail.com>
2024-05-07 15:48:45 -0400

    feat: productview, ui

---
+++ ShoppingSwiftUI/AsyncImageView.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncImageView.swift
+//  ShoppingSwiftUI
+//
+//  Created by Preet Pambhar on 2024-05-07.
+//
+
+import SwiftUI
+
+struct AsyncImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncImageView()
+}
---
+++ ShoppingSwiftUI/ProductDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailsView.swift
+//  ShoppingSwiftUI
+//
+//  Created by Preet Pambhar on 2024-05-07.
+//
+
+import SwiftUI
+
+struct ProductDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailsView()
+}
